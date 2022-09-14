80c3ee68875362095a2652c050d8404cee435326
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-09-14 15:07:57 +0900

    updated with quiz 59

--- dynamicArray.py
+++ dynamicArray.py
@@ -1,17 +1,48 @@
 # Implementation
+import random
+
 class Array:
     def __init__(self, size):
         self.size = size
-        self.data = [None] * size
+        self.data = [None for _ in range(size)]
 
-    def insert(self, index):
+    def __repr__(self):
+        return str(self.data)
+
+    def insert(self, index, value):
         if index < 0 or index >= self.size:
             raise IndexError("Index out of range")
 
-        self.data[index] = index
+        self.data[index] = value
+
+    def indexing(self, index):
+        if index < 0 or index >= self.size:
+            raise IndexError("Index out of range")
+
+        return self.data[index]
 
 
 # Interface example
-x = Array(size=5)   # this is how I want to create an array
-x.insert(0) = 4     # this is how I want to insert an element
-print(x.data)       # this is how I want to read the data
+x = Array(size=10)   # this is how I want to create an array
+for i in range(10):
+    x.insert(index=i, value=random.randint(1, 100))
+
+for i in range(10):
+    print(x.indexing(index=i))
+
+print(x)       # this is how I want to read the data
+
+def test1():
+    count = 0
+
+    for z in range(3):
+        print(f"\nz{z + 1}")
+        
+        for _ in range(5):
+            for _ in range(5):
+                print(count, end=" ")
+                count += 1
+            
+            print()
+
+test1()
